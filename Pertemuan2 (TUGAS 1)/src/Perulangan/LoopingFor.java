6c7a8e5a6d592e32960ddd71e532fea0f4c95371
Meyliasalma <162813299+Meyliasalma@users.noreply.github.com>
2024-03-26 13:07:53 +0700

    Add files via upload

---
+++ Pertemuan2 (TUGAS 1)/src/Perulangan/LoopingFor.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Perulangan;
+
+/**
+ *
+ * @author HP
+ */
+public class LoopingFor {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) 
+    {
+        int nilai = 1;
+
+        do 
+        {
+            System.out.println("nilai: " + nilai);
+            nilai++;
+        } while (nilai < 7);
+    }
+    
+}
---
+++ Pertemuan2 (TUGAS 1)/src/Perulangan/LoopingWhile.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Perulangan;
+
+/**
+ *
+ * @author HP
+ */
+public class LoopingWhile {
+    
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) 
+    {
+        int i = 1;
+        while(i <= 7) {
+            System.out.println("Angka = " + i);
+            i++;
+        }
+    }
+}
